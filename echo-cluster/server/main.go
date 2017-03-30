0847d3d64778657580345262b941f7aae620f6a3
jennal <jennalcn@gmail.com>
2017-03-30 10:42:50 +0800

    use backend service

--- echo-cluster/server/main.go
+++ echo-cluster/server/main.go
@@ -16,23 +16,19 @@ import (
 	"github.com/jennal/goplay-demos/echo-cluster/server/echo"
 	"github.com/jennal/goplay-master/master"
 	"github.com/jennal/goplay/cmd"
-	"github.com/jennal/goplay/service"
 	"github.com/jennal/goplay/transfer/tcp"
 )
 
 func main() {
 	/* setup service  */
 	ser := tcp.NewServer("", 1234)
-	serv := service.NewService("echo", ser)
+	serv := master.NewBackendService("echo", ser)
 
 	/* regist handler */
 	serv.RegistHanlder(echo.NewServices())
 
 	/* link to master */
-	cli := tcp.NewClient()
-	mc := master.NewMasterClient(cli)
-	sp := master.NewServicePack(master.ST_BACKEND, "echo", 1234)
-	mc.Bind(serv, &sp, "", master.PORT)
+	serv.ConnectMaster("", master.PORT)
 
 	cmd.Start(serv)
 }
